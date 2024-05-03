9b69f8cd0afb2133a39327a2f37a81e5d1548a16
DeeBest <simphiwedladla8@gmail.com>
2024-05-03 07:32:16 +0200

    added event listener to the windows to determine scrollY value and change the navbar background accordingly

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const header = document.querySelector('header');
+const container = document.getElementById('container');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > container.offsetTop - header.offsetHeight - 60) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+});
